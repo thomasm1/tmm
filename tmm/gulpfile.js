b995df20857c29f1df0fe945609f176b93c7c628
Thomas M Maestas <thomas76milton@gmail.com>
2017-07-23 13:48:43 -0600

    recovery

---
+++ tmm/gulpfile.js
@@ -0,0 +1,64 @@
+const gulp = require('gulp'),
+      uglify = require('gulp-uglify'),
+      compass = require('gulp-compass'),
+      plumber = require('gulp-plumber'),
+      autoprefixer = require('gulp-autoprefixer'),
+      rename = require('gulp-rename');
+ 
+/* 
+-top-level function-      !css/style.css
+gulp.task -define tasks       css/*.css
+gulp.src - point to files to use  *.+(js|css)
+gulp.dist - points to folder to output 
+gulp.watch - watch files and folders
+*/
+// message log
+gulp.task('message', function(){
+    return console.log('Gulp is running..tmm..');
+});
+// html files
+gulp.task('copyHTML', function(){
+    gulp.src('*.html')
+        .pipe(gulp.dest('dist'));
+});
+// SCRIPTS////////////////////////////////////
+// ['/js/**/**/*.js','/js/**/*.js','/js/*.js','!app/js/**/**/*min.js']
+// js files
+gulp.task('copyJS', function(){
+    gulp.src('js/*.js')
+        .pipe(plumber())
+        .pipe(rename({suffix:'.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+        console.log('JS worked!');
+});
+
+// COMPASS / SASS ////////////////////////////////////
+//  
+gulp.task('compass', function() {
+    gulp.src('sass/*.scss')
+        .pipe(plumber())
+        .pipe(compass({
+            config_file: './config.rb',
+            css: 'css',
+            sass: 'sass',
+            require: ['susy']
+        }))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('dist/scss'));
+});
+// gulp.task('copyCSS', function(){
+  //  gulp.src('css/*.css')
+    //    .pipe(gulp.dest('dist/css'));
+// }); 
+
+// WATCH ////////////////////////////////////
+// 
+gulp.task('watch', function(){
+    gulp.watch('js/*.js', ['copyJS']);
+    gulp.watch('scss/**/*.scss', ['compass']);
+});
+
+// DEFAULT ////////////////////////////////////
+// all files  ['copyCSS', 'copyJS', 'copyHTML']
+gulp.task('default', ['copyJS', 'compass', 'copyHTML']); 
