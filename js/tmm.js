3f98a7bde6451f950fc14b39c20e1e99b8757945
Thomas M Maestas <thomas76milton@gmail.com>
2017-07-23 22:53:12 -0600

    finally finished

--- js/tmm.js
+++ js/tmm.js
@@ -1,9 +1,7 @@
 console.log('hello');
 console.log('im here');
-console.log('yoo-hoo!');
-document.getElementById('title').innerHTML = 'T M M';
-document.getElementById('nav').innerHTML = 'T M +';
+console.log('yoo-hoo!'); 
 document.getElementById('title').innerHTML = 'T M M + + ++++';
-document.getElementsByClassName('mr-3').innerHTML = 'one new nav';
-document.getElementsByClassName('p-4').innerHTML = 'two Nav Bar Newest';
-document.getElementsByClassName('icon').innerHTML = 'gears';
+document.getElementsByClassName('one').innerHTML = 'one ';
+document.getElementsByClassName('two').innerHTML = 'two ';
+document.getElementsByClassName('three').innerHTML = 'three';
