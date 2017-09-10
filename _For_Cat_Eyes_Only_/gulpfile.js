2d914213d383ff7a7ec8cf74a2638e6679b1638e
Thomas M Maestas <thomas76milton@gmail.com>
2017-09-09 21:55:31 -0600

    9/9 edits

---
+++ _For_Cat_Eyes_Only_/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+
+gulp.task('tmm', function(){
+    console.log('My oh  my, it\'s tmm!');
+});
+gulp.task('tmm', function(){
+    console.log('My oh  my, it\'s tmm!');
+});
