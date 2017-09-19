4f18d0483cac16a1ad49fbe0cfe7ebcb06b66b3e
Thomas M Maestas <thomas76milton@gmail.com>
2017-09-19 00:07:06 -0600

    finishing setup

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'breakpoint'
 project_type = :stand_alone
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
-scss_dir = "sass"
-images_dir = "/assets/img"
-fonts_dir = "/assets/fonts"
-javascript_dir = "js"
+sass_dir = "/src/sass"
+scss_dir = "/src/sass"
+images_dir = "/src/img"
+fonts_dir = "/src/fonts"
+javascript_dir = "/src/js"
 line_comments = "false"
 preferred_syntax = :scss
 output_style_ = :expanded
