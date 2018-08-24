531ed9c8ba2bc1cd9981503d90c66570a39142fd
Thomas Maestas <thomas76milton@gmail.com>
2018-08-23 23:16:21 -0600

    services page

--- components/maps.js
+++ components/maps.js
@@ -2,16 +2,11 @@ angular.module('tmm').component('maps', {
   templateUrl:  'views/maps.html',
   controller: function() {
 
-
     this.greeting = 'TMM';
   
     this.toggleGreeting = function() {
       this.greeting = (this.greeting == 'TMM') ? 'Our Daily Tech Post' :  'TMM';
-    }
-
-
-
-
+    } 
   }
 })
  
