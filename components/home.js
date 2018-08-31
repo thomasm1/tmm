189237681177561fd104f2618ecc7a9a69581768
Thomas Maestas <tmaestas@xpansivdata.com>
2018-08-31 08:09:43 -0600

    post-detail redesign

--- components/home.js
+++ components/home.js
@@ -2,11 +2,11 @@ angular.module('tmm').component('home', {
   templateUrl:  'views/home.html',
   controller: 'homeController',
   controllerAs: 'homeCtrl', function() {
-    homeCtrl.greeting = 'TMM';
+    homeCtrl.greeting = 'Welcome to TMM';
   
     homeCtrl.toggleGreeting = function() {
-      homeCtrl.greeting = (homeCtrl.greeting == 'TMM') ? 'Our Daily Tech Post' :  'TMM';
-    }
+      homeCtrl.greeting = (homeCtrl.greeting == 'Welcome to TMM') ? 'Thomas M. Maestas' :  'Welcome to TMM';
+    };
   }
-})
+});
  
