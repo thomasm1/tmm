77eb13bc27f1902c79c1f9c98b1a006a8e986075
Thomas Maestas <thomas76milton@gmail.com>
2019-01-06 21:52:28 -0700

    webpack added babel->ES2015, working fine

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
-require('babel-loader');
+//require('babel-loader');
 const path = require('path');  
 
 module.exports = {
   entry: {
-    app: [ 
+    app: [
+      'babel-polyfill',
       './public/src/js/app.js' 
     ],
   },
   output: {
     path: path.resolve(__dirname, './public/dist'),
     filename: 'tmm_bundle.js',
-  }
-/*  ,
+  },
   module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
+    loaders: [{
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+           presets: ['env', 'stage-0']
+        }
+    }]
   }
-  */
-};
+}
+ 
